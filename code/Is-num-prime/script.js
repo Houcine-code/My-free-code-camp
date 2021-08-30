5b5fabf2327ebdc0e5665da0c21fcf469281dc65
Houcine <76647949+Houcine-code@users.noreply.github.com>
2021-08-30 16:59:04 +0100

    Update script.js

--- code/Is-num-prime/script.js
+++ code/Is-num-prime/script.js
@@ -1,12 +1,13 @@
-function checkprimes(num) {
-  let d = 0,status = 0;
-  for(let i = 2; i < num; i++){
-    if(num % i == 0){ 
+function checkprimes(n) {
+  let d = 0,status;
+  for(let i = 2; i < n; i++){
+    if(n % i == 0){ 
       d++;
     }
   }
   if(d) status = false;
+  else if (n == 1) status = false;
   else status = true;
-  return status
+  return status;
 }
-console.log(checkprimes(10));
+console.log(checkprimes(9));
