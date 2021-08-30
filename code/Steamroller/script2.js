014df2795c6151dd9f7f673ece80b5af482762c7
Houcine <76647949+Houcine-code@users.noreply.github.com>
2021-08-30 23:11:44 +0100

    Create script2.js

---
+++ code/Steamroller/script2.js
@@ -0,0 +1,5 @@
+function steamrollArray(arr, d = 1) {
+   return d > 0 ? arr.reduce((acc, val) => acc.concat(Array.isArray(val) ? flatDeep(val, d - 1) : val), [])
+                : arr.slice();
+};
+steamrollArray([1, {}, [3, [[4]]]],4);
